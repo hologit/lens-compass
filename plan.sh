7c8692ca38b3d3b2dc6d23cb6423664a10395531
Chris Alfano <chris@jarv.us>
2019-09-28 18:52:14 -0400

    refactor: externalize lens-tree script

--- plan.sh
+++ plan.sh
@@ -22,30 +22,9 @@ do_install() {
   build_line "Generating lens script"
 
   pushd "${pkg_prefix}" > /dev/null
-  cat > "bin/lens-tree" <<- EOM
-#!$(pkg_path_for bash)/bin/sh
-
-INPUT_TREE="\${1?<input> required}"
-
-# redirect all output to stderr
-{
-  # export git tree to disk
-  git holo lens export-tree "\${INPUT_TREE}"
-
-  # execute compilation
-  pushd "\${GIT_WORK_TREE}/sass" > /dev/null
-  compass compile
-  popd > /dev/null
-
-  # add output to git index
-  git add css
-} 1>&2
-
-# output tree hash
-git write-tree --prefix=css
-
-EOM
-  chmod +x "bin/lens-tree"
+  cp "${PLAN_CONTEXT}/lens-tree" "bin/"
+  fix_interpreter "bin/*" core/bash bin/bash
+  chmod +x "bin/"*
   popd > /dev/null
 }
 
